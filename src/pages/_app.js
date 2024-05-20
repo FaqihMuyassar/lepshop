5b76c56f8c8b841fa67d7a650189a3116c023909
ahfary <afabqory@gmail.com>
2024-05-20 07:48:25 +0700

    ngapus cart provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
 import "../styles/global.css"
-import { CartProvider } from '../assets/components/cartContext.jsx';
 
 export default function App({ Component, pageProps }) {
   return (
-    <CartProvider>
       <Component {...pageProps} />
-    </CartProvider>
   )
 }
