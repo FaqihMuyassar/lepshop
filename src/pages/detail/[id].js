36bf1c59edbeacb71b1aa60624f856fa8d6e4c9e
FaqihMuyassar <faqih16mysr@gmail.com>
2024-05-20 08:17:06 +0700

    responsif untuk detail produk

--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -14,13 +14,13 @@ const Dynamic = ({addToCart}) => {
             {filterData.map((item, index) => (
         <div key={index}>
           <Navigasibar />
-          <div className="flex gap-24 h-full px-56 py-12">
-            <div className="w-1/2 flex justify-center items-center">
+          <div className="flex w-full md:gap-24 gap-4 h-full md:px-56 px-4 py-12 md:flex-row flex-col">
+            <div className="md:w-1/2 w-full flex justify-center items-center">
               <img src={item.image} className="bg-cover" />
             </div>
-            <div className="w-1/2 ml-24">
+            <div className="md:w-1/2 md:ml-24 ml-0 w-full">
               <h1 className="text-4xl font-semibold pb-2">{item.title}</h1>
-              <p className="text-slate-500 text-lg w-3/4">
+              <p className="text-slate-500 text-lg md:w-3/4 w-full">
                 Bring ideas to life at a blistering pace with our most powerful
                 13″ laptop—featuring 12th Gen Intel® Core™ processors and
                 innovative, modern design.
@@ -33,7 +33,7 @@ const Dynamic = ({addToCart}) => {
                   16 GB / 1 TB
                 </button>
               </div>
-              <h1 className="text-4xl font-semibold py-8">{item.price}</h1>
+              <h1 className="text-4xl font-semibold py-8 md:pt-0 pt-24">{item.price}</h1>
               <div className="flex gap-4">
                 <button className="bg-black rounded text-white text-center w-56 py-3 text-lg" onClick={() => addToCart(id)}>
                   Add to Cart
@@ -44,10 +44,10 @@ const Dynamic = ({addToCart}) => {
               </div>
             </div>
           </div>
-          <div className="pb-24 pt-12 px-56">
+          <div className="pb-24 pt-12 md:px-56 px-4 w-full">
             <h1 className="font-semibold text-4xl">Specification</h1>
-            <div className="flex gap-24 pt-8">
-              <div className="w-1/2">
+            <div className="flex md:flex-row flex-col md:gap-24 gap-2 pt-8">
+              <div className="md:w-1/2 w-full">
                 <div>
                   <h2 className="font-semibold text-xl">Processor</h2>
                   <p className="text-slate-500 text-lg">{item.Specification.processor}</p>
@@ -55,7 +55,7 @@ const Dynamic = ({addToCart}) => {
                 <div>
                   <h2 className="font-semibold text-xl">Operating System</h2>
                   <p className="text-slate-500 text-lg">
-                    {item.Specification.operatingSystem}
+                    {item.Specification.opertingSystem}
                   </p>
                 </div>
                 <div>
@@ -71,7 +71,7 @@ const Dynamic = ({addToCart}) => {
                   </p>
                 </div>
               </div>
-              <div className="w-1/2 ml-24">
+              <div className="md:w-1/2 w-full md:ml-24 ml-0">
                 <div>
                   <div>
                     <h2 className="font-semibold text-xl">Memory</h2>
