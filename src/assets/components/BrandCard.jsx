9843416e456714ea55fadcd2ee57b75ce3a831de
FaqihMuyassar <faqih16mysr@gmail.com>
2024-05-21 09:11:50 +0700

    nambah data

--- src/assets/components/BrandCard.jsx
+++ src/assets/components/BrandCard.jsx
@@ -3,8 +3,9 @@ export const BrandCard = ({id, image, href}) => {
   return (
     <>
       <a href={href} target='_blank' className="shadow-lg rounded-lg md:w-40 md:h-40 w-24 h-24 flex justify-center items-center" id={id}>
-        <img src={image} className='md:w-20 w-14'/>
+        <img src={} className='md:w-20 w-14'/>
       </a>
     </>
   );
 };
+image
