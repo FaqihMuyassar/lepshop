ee5ae3892255ff464a913d074eb44c1e09c87278
FaqihMuyassar <faqih16mysr@gmail.com>
2024-06-11 14:25:48 +0700

    f

--- src/assets/components/Adv.jsx
+++ src/assets/components/Adv.jsx
@@ -5,7 +5,7 @@ export const Adv = ( {id, title, date, image, height, width} ) => {
     return (
         <div onClick={MyPopup} className={`shadow-xl ${width} ${height} flex flex-col pb-3 md:rounded-xl rounded-none overflow-hidden `}id={id}>
         <div className="bg-slate-200 w-full flex justify-center items-center h-3/4">
-          <Image src={image} className="w-full h-full" width={30} height={213} />
+          <Image src={image} className="w-full h-full" width={30} height={1} />
         </div>
         <div className="px-4 h-1/4 pt-2">
         <p className="text-slate-400 text-xs dark:text-white">{date}</p>
