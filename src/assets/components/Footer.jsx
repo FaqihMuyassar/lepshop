0e13216a2628f206b022819ed33e93aad7f75f98
FaqihMuyassar <faqih16mysr@gmail.com>
2024-06-16 16:13:58 +0700

    sali

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -7,40 +7,40 @@ export const Footer = () => {
             <div id="Komunitas" className="md:w-1/5 w-full">
               <h3 className="text-xl font-bold text-gray-500 dark:text-white">KOMUNITAS</h3>
               <div className="flex flex-col gap-2 pt-2">
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Youtube</a>
-                <a target="/" href="www.instagram.com/ahfary_" className="text-sm text-gray-400 dark:text-white">Instagram</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Facebook</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Team Gaming</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Blog</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Youtube</a>
+                <a target="_blank" href="www.instagram.com/ahfary_" className="text-sm text-gray-400 dark:text-white">Instagram</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Facebook</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Team Gaming</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Blog</a>
               </div>
             </div>
             <div id="Dukungan" className="md:w-1/5 w-full">
               <h3 className="text-xl font-bold text-gray-500 dark:text-white">DUKUNGAN</h3>
               <div className="flex flex-col gap-2 pt-2">
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Customer Service Online</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Lokasi Service</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Garansi</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Layanan Jarak Jauh</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Customer Service Online</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Lokasi Service</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Garansi</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Layanan Jarak Jauh</a>
               </div>
             </div>
             <div id="Berita" className="md:w-1/5 w-full">
               <h3 className="text-lg font-bold text-gray-500 dark:text-white">BERITA</h3>
               <div className="flex flex-col gap-2 pt-2">
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Whats New</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Awards</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Whats New</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Awards</a>
               </div>
             </div>
             <div id="TentangKami" className="md:w-1/5 w-full">
               <h3 className="text-xl font-bold text-gray-500 dark:text-white">TENTANG KAMI</h3>
               <div className="flex flex-col gap-2 pt-2">
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Tentang MSI</a>
-                <a target="/" className="text-sm text-gray-400 dark:text-white">Visi Dan Misi</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Tentang MSI</a>
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">Visi Dan Misi</a>
               </div>
             </div>
             <div id="Address" className="md:w-1/5 w-full">
               <h3 className="text-xl font-bold text-gray-500 dark:text-white">ALAMAT</h3>
               <div className="flex flex-col gap-2 pt-2 w-56">
-                <a target="/" className="text-sm text-gray-400 dark:text-white">
+                <a target="_blank" className="text-sm text-gray-400 dark:text-white">
                   Kp.Kebon Kelapa, RT.02/RW.11, Singasari, Kec.Jonggol, Bogor,
                   Jawa Barat 16830
                 </a>
